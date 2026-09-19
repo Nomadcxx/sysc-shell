5674306165b9daddd7ea35b03351d42b601a14fc
Nomadcxx <noovie@gmail.com>
2026-09-20 04:47:11 +1000

    feat(plugin): expose the radial gauge to plugins

--- internal/plugin/supervisor.go
+++ internal/plugin/supervisor.go
@@ -198,7 +198,7 @@ func (s *Supervisor) reap(sess *Session) {
 func (s *Supervisor) handshake(ctx context.Context, sess *Session) error {
 	granted := intersect(s.Manifest.Capabilities, s.Supported)
 	hello := &v1.HostHello{
-		Supported:    []v1.Version{{Major: 1, Minor: 2}},
+		Supported:    []v1.Version{{Major: 1, Minor: 3}},
 		Plugin:       v1.Identity{ID: s.Manifest.ID, Name: s.Manifest.Name, Version: s.Manifest.Version},
 		Capabilities: capabilityNames(granted),
 		Limits:       s.Limits,
